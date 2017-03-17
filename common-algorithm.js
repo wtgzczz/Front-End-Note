76c8d72366ed613a7762b341557f8d4ec68f3959
Tommy <694008175@qq.com>
2017-03-17 17:24:50 +0800

    Update common-algorithm.js
    add heap sort

--- common-algorithm.js
+++ common-algorithm.js
@@ -424,6 +424,47 @@ function insertionSort(arr){
 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 */
 
+// 堆排序
+function heapSort(array){
+	if(Object.prototype.toString.call(array).slice(8,-1) === 'Array'){
+		// 初始化堆
+		var heapSize = array.length,temp;
+		  // 从最后一个非叶子节点开始
+		for(var i = Math.floor(heapSize / 2) - 1;i >= 0; i--){
+			heapify(array,i,heapSize);
+		}
+		// 把顶端的节点和有序区的节点交换，对无序区进行位置的调整
+		for(var j  = heapSize - 1;j >= 1; j--){
+			temp = array[0];
+			array[0] = array[j];
+			array[j] = temp;
+			heapify(array,0,--heapSize);
+		}
+	}else{
+		return 'array is not an Array';
+	}
+}
+// 调整堆位置的过程
+function heapify(arr,x,len){
+	if(Object.prototype.toString.call(arr).slice(8,-1) === 'Array' && typeof x === 'number'){
+		var l = 2 * x + 1,r = 2 * x + 2,largest = x,temp;
+		if(l < len && arr[l] > arr[largest]){
+			largest = l;
+		} 
+		if(r < len && arr[r] > arr[largest]){
+			largest = r;
+		}
+		if(largest != x) {
+			temp = arr[x];
+			arr[x] = arr[largest];
+			arr[largest] = temp;
+			heapify(arr, largest, len);
+		}
+	}else{
+		return 'arr is not an Array or x is not a number'
+	}
+}	
+	
 /*归并排序
 把一系列排好序的子序列合并成一个大的完整有序序列。
 从最小的单位开始合并。然后再逐步合并合并好的有序数组。最终实现归并排序。 
